ecd33db485ac47c2aef7298f2ddd6d3ee702955a
aliza614 <47804976+aliza614@users.noreply.github.com>
2020-03-08 19:58:05 -0600

    Create game-pong.py

---
+++ game-pong.py
@@ -0,0 +1,10 @@
+import turtle
+
+window = turtle.Screen()
+window.bgcolor("black")
+window.title("Pong by Aliza")
+window.setup(width=800, height=600)
+
+#keeps the window from closing automatically
+import Tkinter
+Tkinter.mainloop()
