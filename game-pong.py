9aef88dba43f10e17a5d4a73253e813e70b31bce
aliza614 <47804976+aliza614@users.noreply.github.com>
2020-03-10 21:36:22 -0600

    adding left paddle

--- game-pong.py
+++ game-pong.py
@@ -5,6 +5,14 @@ window.bgcolor("black")
 window.title("Pong by Aliza")
 window.setup(width=800, height=600)
 
+left_paddle=turtle.Turtle()
+left_paddle.speed(0)
+left_paddle.shape("square")
+left_paddle.color("blue")
+left_paddle.shapesize(stretch_wid=5, stretch_len=1)
+left_paddle.penup()
+left_paddle.goto(-360, 0)
+
 #keeps the window from closing automatically
 import tkinter
 import _tkinter
