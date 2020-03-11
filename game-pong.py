bd21efdf6afb604fa7d3c7d3c4896b32dee68a44
aliza614 <47804976+aliza614@users.noreply.github.com>
2020-03-11 10:37:49 -0600

    getting the main comments in

--- game-pong.py
+++ game-pong.py
@@ -5,6 +5,11 @@ window.bgcolor("black")
 window.title("Pong by Aliza")
 window.setup(width=800, height=600)
 
+#score
+left_score=0
+right_score=0
+
+#left_paddle
 left_paddle=turtle.Turtle()
 left_paddle.speed(0)
 left_paddle.shape("square")
@@ -13,7 +18,7 @@ left_paddle.shapesize(stretch_wid=5, stretch_len=1)
 left_paddle.penup()
 left_paddle.goto(-360, 0)
 
-
+#right paddle
 right_paddle=turtle.Turtle()
 right_paddle.speed(0)
 right_paddle.shape("square")
@@ -22,6 +27,36 @@ right_paddle.shapesize(stretch_wid=5, stretch_len=1)
 right_paddle.penup()
 right_paddle.goto(360, 0)
 
+#ball
+ball=turtle.Turtle()
+ball.shape=("square")
+ball.penup()
+ball.goto(0,0)
+ball.dx=2
+ball.dy=2
+
+#scoreboard
+scoreboard=turtle.Turtle()
+scoreboard.color("black")
+scoreboard.penup()
+scoreboard.hideturtle()
+scoreboard.goto(0,260)
+scoreboard.clear()
+scoreboard.write("Player A: 0 || Player B: 0",align="center", font=("Arial", 24, "normal")
+
+#functions
+##left paddle up
+##left paddle down
+##right paddle up
+## right paddle down
+
+#keyboard listeners
+                 
+#main game loop
+    #move ball
+    #bounce ball
+    #ball goes off screen
+    #ball collides with paddles
 
 
 #keeps the window from closing automatically
