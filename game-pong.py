d4dd3052788893f8b69f9adc900f865049107be1
aliza614 <47804976+aliza614@users.noreply.github.com>
2020-03-10 21:39:09 -0600

    add right paddle

--- game-pong.py
+++ game-pong.py
@@ -13,6 +13,17 @@ left_paddle.shapesize(stretch_wid=5, stretch_len=1)
 left_paddle.penup()
 left_paddle.goto(-360, 0)
 
+
+right_paddle=turtle.Turtle()
+right_paddle.speed(0)
+right_paddle.shape("square")
+right_paddle.color("red")
+right_paddle.shapesize(stretch_wid=5, stretch_len=1)
+right_paddle.penup()
+right_paddle.goto(360, 0)
+
+
+
 #keeps the window from closing automatically
 import tkinter
 import _tkinter
